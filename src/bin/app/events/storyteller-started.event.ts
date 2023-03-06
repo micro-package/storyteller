ae34dbe5ffecdaa3fefbf4b2f2a5ff6d8921498e
lukasz.klejszta <lukasz.klejszta@tsh.io>
2023-03-07 00:06:43 +0100

    feat: remove cli from storyteller

--- src/bin/app/events/storyteller-started.event.ts
+++
@@ -1,9 +0,0 @@
-import { zod } from "../../shared/zod";
-import { EventName, eventValidator } from "../event";
-
-export const eventValidatorStorytellerStarted = eventValidator.extend({
-  eventName: zod.literal(EventName.storytellerStarted),
-  eventPayload: zod.object({
-    plugins: zod.string().array(),
-  }),
-});
--- src/bin/app/events/test-start-requested.event.ts
+++
@@ -1,9 +0,0 @@
-import { zod } from "../../shared/zod";
-import { EventName, eventValidator } from "../event";
-
-export const eventValidatorTestStartRequested = eventValidator.extend({
-  eventName: zod.literal(EventName.storytellerFinished),
-  eventPayload: zod.object({
-    testNames: zod.string().array(),
-  }),
-});
