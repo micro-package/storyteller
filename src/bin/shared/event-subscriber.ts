3743bc9d6b88e4277cc00858c1f727b14aeed047
lukasz.klejszta <lukasz.klejszta@tsh.io>
2023-03-06 18:26:03 +0100

    feat: add command bus

--- src/bin/shared/event-subscriber.ts
+++ src/bin/shared/event-subscriber.ts
@@ -1,16 +1,36 @@
 import type { eventValidator } from "../app/event";
+import type { zod } from "./zod";
 
-export interface EventSubscriber<TFunctions extends { [key in string]: (event: any) => Promise<void> }> {
-  subscriptions: { eventFunction: TFunctions[keyof TFunctions]; eventValidator: typeof eventValidator }[];
+export interface EventSubscriber<
+  TValidators extends typeof eventValidator,
+  TEvents extends zod.infer<TValidators>,
+  TFunctions extends {
+    [key in string]: (event: TEvents) => Promise<void>;
+  },
+> {
+  subscriptions: {
+    eventFunction: {
+      [key in keyof TFunctions]: (
+        event: TEvents extends { eventName: infer UEventName } ? Extract<TEvents, UEventName> : never,
+      ) => Promise<void>;
+    }[keyof TFunctions];
+    eventValidator: TValidators;
+  }[];
   functions: TFunctions;
 }
 
-export const eventSubscriber = <TFunctions extends { [key in string]: (event: any) => Promise<void> }>(
+export const createEventSubscriber = <
+  TValidators extends typeof eventValidator,
+  TEvents extends zod.infer<TValidators>,
+  TFunctions extends {
+    [key in string]: (event: TEvents) => Promise<void>;
+  },
+>(
   subscriptions: (
     functions: TFunctions,
-  ) => { eventFunction: TFunctions[keyof TFunctions]; eventValidator: typeof eventValidator }[],
+  ) => { eventFunction: TFunctions[keyof TFunctions]; eventValidator: TValidators }[],
   functions: TFunctions,
-): EventSubscriber<TFunctions> => ({
+): EventSubscriber<TValidators, TEvents, TFunctions> => ({
   subscriptions: subscriptions(functions),
   functions,
 });
