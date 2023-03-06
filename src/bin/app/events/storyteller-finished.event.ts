15e3db7c31b9123b398ebe0d86ad14b1e6928f74
lukasz.klejszta <lukasz.klejszta@tsh.io>
2023-03-07 00:06:43 +0100

    feat: remove cli from storyteller

--- src/bin/app/events/storyteller-finished.event.ts
+++
@@ -1,9 +0,0 @@
-import { zod } from "../../shared/zod";
-import { EventName, eventValidator } from "../event";
-
-export const eventValidatorStorytellerFinished = eventValidator.extend({
-  eventName: zod.literal(EventName.storytellerFinished),
-  eventPayload: zod.object({
-    storiesFinished: zod.string().array(),
-  }),
-});
