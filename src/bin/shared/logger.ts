0d26a1f395e53a5d2d3205a8b5f2c06ba9c86aa6
lukasz.klejszta <lukasz.klejszta@tsh.io>
2023-03-07 00:06:43 +0100

    feat: remove cli from storyteller

--- src/bin/shared/logger.ts
+++
@@ -1,8 +0,0 @@
-export const logger = {
-  // eslint-disable-next-line no-console
-  error: console.log,
-  // eslint-disable-next-line no-console
-  debug: console.log,
-  // eslint-disable-next-line no-console
-  warn: console.log,
-};
