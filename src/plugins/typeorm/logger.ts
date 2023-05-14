6fbc33922d1fec3e311abc55679f2dc7127e572d
lukasz.klejszta <lukasz.klejszta@tsh.io>
2023-05-14 19:21:46 +0200

    feat: remove query runner from typeorm

--- src/plugins/typeorm/logger.ts
+++ src/plugins/typeorm/logger.ts
@@ -1,49 +1,49 @@
-import type { Logger, QueryRunner } from "typeorm";
+import type { Logger } from "typeorm";
 import type { TypeormDataSource, TypeormValueObject } from "./types";
 import { TypeormHookName } from "./types";
 
 export class TypeormPluginLogger implements Logger {
   constructor(public valueObject: TypeormValueObject<string>, public dataSource: TypeormDataSource<string>) {}
 
-  log(query: string, parameters?: any[], queryRunner?: QueryRunner) {
+  log(query: string, parameters?: any[]) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "log", query, parameters, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "log", query, parameters, dataSourceName: this.dataSource.name },
     });
   }
 
-  logMigration(message: string, queryRunner?: QueryRunner) {
+  logMigration(message: string) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "logMigration", message, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "logMigration", message, dataSourceName: this.dataSource.name },
     });
   }
 
-  logQuery(query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner) {
+  logQuery(query: string, parameters?: any[] | undefined) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "logQuery", query, parameters, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "logQuery", query, parameters, dataSourceName: this.dataSource.name },
     });
   }
 
-  logQueryError(error: string | Error, query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner) {
+  logQueryError(error: string | Error, query: string, parameters?: any[] | undefined) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "logQueryError", error, query, parameters, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "logQueryError", error, query, parameters, dataSourceName: this.dataSource.name },
     });
   }
 
-  logQuerySlow(time: number, query: string, parameters?: any[] | undefined, queryRunner?: QueryRunner) {
+  logQuerySlow(time: number, query: string, parameters?: any[] | undefined) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "logQuerySlow", time, query, parameters, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "logQuerySlow", time, query, parameters, dataSourceName: this.dataSource.name },
     });
   }
 
-  logSchemaBuild(message: string, queryRunner?: QueryRunner) {
+  logSchemaBuild(message: string) {
     void this.valueObject.runHooks({
       name: TypeormHookName.ormLogged,
-      payload: { name: "logSchemaBuild", message, queryRunner, dataSourceName: this.dataSource.name },
+      payload: { name: "logSchemaBuild", message, dataSourceName: this.dataSource.name },
     });
   }
 }
